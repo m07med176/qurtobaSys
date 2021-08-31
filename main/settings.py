002999b5c03bba4365b8020969b24af4d6aa39e5
m07med176 <mohamed.arafa176@gmail.com>
2021-08-31 21:43:08 +0200

    time zone

--- main/settings.py
+++ main/settings.py
@@ -137,7 +137,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'ar'
-TIME_ZONE = 'GMT+2'
+TIME_ZONE = 'Africa/Cairo'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
