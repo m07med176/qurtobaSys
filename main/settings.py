a2ba1b5950a3aa22d766fe64305151cdb68cb834
m07med176 <mohamed.arafa176@gmail.com>
2021-10-19 13:36:19 +0200

    login

--- main/settings.py
+++ main/settings.py
@@ -61,12 +61,12 @@ REST_FRAMEWORK = {
     
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':16,
-
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework.authentication.TokenAuthentication',
+        ),
     
 }
-# 'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework.authentication.TokenAuthentication',
-#     ),
+
 #     'DEFAULT_PERMISSION_CLASSES': (
 #         'rest_framework.permissions.IsAuthenticated',
 #     ),
