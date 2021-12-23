653ad20d633c7163d39e7985183db4305d279ad3
m07med176 <mohamed.arafa176@gmail.com>
2021-12-23 20:03:03 +0200

    rport

--- transactions/api/controllers/reports.py
+++ transactions/api/controllers/reports.py
@@ -73,14 +73,15 @@ def getReports(request):
         pd      = LogDate.objects.get(pk=int(id)-1).datetime.astimezone(tz)
         date1   = d.strftime("%m/%d/%Y %I:%M:%p")
         date2   = pd.strftime("%m/%d/%Y %I:%M:%p")
-        results = Record.objects.filter(datetime__range = (str(d),str(pd))).order_by('pk')
-        rest    = get_rest(results.distinct('customerData'))
+        results = Record.objects.filter(datetime__range = (str(d),str(pd))) #.order_by('-datetime')
+        rest    = get_rest(results.distinct('customerData_id'))
     
     elif df != None and dt != None:
         date1   = df
         date2   = dt
-        results = Record.objects.filter(date__range = (df,dt)).order_by('pk')
-        rest    = get_rest(results.distinct('customerData'))
+        results = Record.objects.filter(date__range = (df,dt)) #.order_by('-datetime')
+        data = results.distinct('customerData_id')
+        rest  = get_rest(data)
 
     elif dtf != None and dtt != None:
         dtf = dtf.replace(" ","+")
@@ -91,16 +92,18 @@ def getReports(request):
         date_to_obj = datetime.datetime.fromisoformat(dtt)
         date1 = date_from_obj.strftime("%m/%d/%Y %I:%M:%p")
         date2 = date_to_obj.strftime("%m/%d/%Y %I:%M:%p")
-        results = Record.objects.filter(datetime__range = (str(date_from_obj),str(date_to_obj))).order_by('pk')
-        rest  = get_rest(results.distinct('customerData'))
+        results = Record.objects.filter(datetime__range = (str(date_from_obj),str(date_to_obj))) #.order_by('-datetime')
+        data = results.distinct('customerData_id')
+        rest  = get_rest(data)
 
     elif dtfn != None and dttn != None:
         date_from_obj = datetime.datetime.strptime(dtfn, '%Y-%m-%d %H:%M:%S')
         date_to_obj = datetime.datetime.strptime(dttn, '%Y-%m-%d %H:%M:%S')
         date1 = date_from_obj.strftime("%m/%d/%Y %I:%M:%p")
         date2 = date_to_obj.strftime("%m/%d/%Y %I:%M:%p")
-        results = Record.objects.filter(datetime__range = (str(date_from_obj),str(date_to_obj))).order_by('pk')
-        rest  = get_rest(results.distinct('customerData'))
+        results = Record.objects.filter(datetime__range = (str(date_from_obj),str(date_to_obj))) # .order_by('-datetime')
+        data = results.distinct('customerData_id')
+        rest  = get_rest(data)
    
     else:
         ld = LogDate.objects.order_by('id').last().datetime.astimezone(tz)
