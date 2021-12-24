ac9c914fd732dd2876625dc2d00e94de4930f94c
m07med176 <mohamed.arafa176@gmail.com>
2021-12-24 10:28:10 +0200

    report

--- transactions/api/controllers/reports.py
+++ transactions/api/controllers/reports.py
@@ -74,13 +74,14 @@ def getReports(request):
         date1   = d.strftime("%m/%d/%Y %I:%M:%p")
         date2   = pd.strftime("%m/%d/%Y %I:%M:%p")
         results = Record.objects.filter(datetime__range = (str(d),str(pd))) #.order_by('-datetime')
-        rest    = get_rest(results.distinct('customerData_id'))
+        data = Record.objects.filter(datetime__range = (str(d),str(pd))).order_by('-datetime').distinct('customerData_id')
+        rest    = get_rest(data)
     
     elif df != None and dt != None:
         date1   = df
         date2   = dt
         results = Record.objects.filter(date__range = (df,dt)) #.order_by('-datetime')
-        data = results.distinct('customerData_id')
+        data = Record.objects.filter(date__range = (df,dt)).order_by('-datetime').distinct('customerData_id')
         rest  = get_rest(data)
 
     elif dtf != None and dtt != None:
@@ -93,7 +94,7 @@ def getReports(request):
         date1 = date_from_obj.strftime("%m/%d/%Y %I:%M:%p")
         date2 = date_to_obj.strftime("%m/%d/%Y %I:%M:%p")
         results = Record.objects.filter(datetime__range = (str(date_from_obj),str(date_to_obj))) #.order_by('-datetime')
-        data = results.distinct('customerData_id')
+        data = Record.objects.filter(datetime__range = (str(date_from_obj),str(date_to_obj))).order_by('-datetime').distinct('customerData_id')
         rest  = get_rest(data)
 
     elif dtfn != None and dttn != None:
@@ -102,7 +103,7 @@ def getReports(request):
         date1 = date_from_obj.strftime("%m/%d/%Y %I:%M:%p")
         date2 = date_to_obj.strftime("%m/%d/%Y %I:%M:%p")
         results = Record.objects.filter(datetime__range = (str(date_from_obj),str(date_to_obj))) # .order_by('-datetime')
-        data = results.distinct('customerData_id')
+        data = Record.objects.filter(datetime__range = (str(date_from_obj),str(date_to_obj))).order_by('-datetime').distinct('customerData_id')
         rest  = get_rest(data)
    
     else:
