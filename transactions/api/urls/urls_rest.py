62f99c74019a0435e681c077974d3865cfe98df1
m07med176 <mohamed.arafa176@gmail.com>
2021-12-25 19:28:23 +0200

    rest

--- transactions/api/urls/urls_rest.py
+++ transactions/api/urls/urls_rest.py
@@ -5,7 +5,6 @@ from transactions.api.controllers import mainRest as api
 urlpatterns = [
         # region Main Rest
         path('api/getRest/<str:name>/',api.GetRest.as_view()),
-        path('api/getRestDateCalc/',api.getRestDateCalc),
         # seller should pay
         path('restSeller/<str:email>/',api.getSellerRest),
         path('restAssistance/<str:email>/',api.getAssistanceRest),
