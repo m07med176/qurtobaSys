15becb307371017bd05deb984b9e448aac5f3fa3
m07med176 <mohamed.arafa176@gmail.com>
2022-01-02 16:59:55 +0200

    last edit

--- FollowUpApp/api/serializers.py
+++ FollowUpApp/api/serializers.py
@@ -10,7 +10,7 @@ class SEmployersAll(serializers.ModelSerializer):
     duration = serializers.SerializerMethodField('get_duration')
     class Meta:
         model = Employers
-        fields =['uid','email','name','image','phone','date_joined','last_login','is_superuser','is_admin','is_staff','is_active','type','duration']
+        fields =['id','uid','email','name','image','phone','date_joined','last_login','is_superuser','is_admin','is_staff','is_active','type','duration']
     def get_duration(self,employer):
         duration = FollowUp.objects.filter(user=employer).first()
         return SFollowUp(duration).data
