3e435ea911e8bf2179d590014e7d5db0963a6a53
m07med176 <mohamed.arafa176@gmail.com>
2022-01-06 13:35:06 +0200

    duration

--- FollowUpApp/api/pagination.py
+++ FollowUpApp/api/pagination.py
@@ -11,15 +11,36 @@ class CustomPagination(PageNumberPagination):
     month = datetime.now().date().month
     year = datetime.now().date().year
     days  = monthrange(year=year,month= month)[1]
+    constantDuration = [str(datetime.now().date().year)+"-"+str(datetime.now().date().month).zfill(2)+"-"+str(i).zfill(2) for i in range(1,days+1)]
+    constantDuration.reverse()
     def get_paginated_response(self, data):
+        dataL = [d["day"] for d in data]
+        customData = []
+        for date in self.constantDuration:
+            if date in dataL:
+                item = data[dataL.index(date)]
+            else:
+                item =  {
+                    "id": 0,
+                    "user": None,
+                    "is_active": False,
+                    "day": date,
+                    "startTime": "",
+                    "endTime": "",
+                    "duration": "",
+                    "dateTime": "",
+                    "transport": 0,
+                    "notes": ""
+                    }
+            customData.append(item)
+
         return Response({
             'links': {
                 'next': self.get_next_link(),
                 'previous': self.get_previous_link()
             },
-            'days':self.days,
             'count': self.page.paginator.count,
             'page_size': self.page_size,
-            'results': data,
+            'results': customData,
             
         })
